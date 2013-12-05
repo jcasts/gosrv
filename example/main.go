013823ac864a1f47cc84f8fda897703f938be277
Jeremie Castagna <jcastagna@twitter.com>
2013-12-05 13:55:57 -0800

    Example file

---
+++ example/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+  "os"
+  "time"
+  "../../gosrv"
+)
+
+func main() {
+  s, err := gosrv.NewServerFromFlag()
+
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+
+  s.HandleFunc("/", func(wr http.ResponseWriter, req *http.Request){
+    wr.Write([]byte("Hello World!\n"))
+  })
+
+  s.OnStop(func()error{
+    fmt.Println("Sleeping for 1s")
+    time.Sleep(1 * time.Second)
+    return nil
+  })
+
+  s.ListenAndServe()
+}
